39d4e585c7b1e182c3af0151f4743e623aebe3ae
Michael Zhang <mail@mzhang.io>
2025-03-22 22:07:02 +0000

    toolpie solve

--- forensics/toolpie/decrypt.py
+++ forensics/toolpie/decrypt.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 k = "5UUfizsRsP7oOCAq".encode()
 c = AES.new(k , AES.MODE_CBC, k)
@@ -7,6 +8,7 @@ with open("extracted-file", "rb") as f:
     data = f.read()
 
 result = c.decrypt(data)
+result = unpad(result, 16)
 
 with open("decrypted-file", "wb") as f:
     f.write(result)
